092365a43427dc378ee5161ee02f9e3f8df55391
abinashofficial <abinash1411999@gmail.com>
2025-11-01 02:10:49 +0530

    added petcare design

--- src/pages/webdesign.tsx
+++ src/pages/webdesign.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Lottie from "lottie-react";
 // import coinEmoji from "../assets/animations/coin.json";
 // import windowsicon from "../assets/animations/windows.json"
@@ -9,7 +9,7 @@ import Lottie from "lottie-react";
         import Coins from "./coins"
 import Header from '../components/header';
 import { IoSearch } from "react-icons/io5";
-import LawAnime from "../assets/animations/law.json"
+// import LawAnime from "../assets/animations/law.json"
 
 
 
@@ -29,213 +29,415 @@ interface GameSpecs {
   download_link: any;
   image_link: any;
   platform: any;
+  json_link:any;
+  page_link:any;
 }
 
-// interface Animations {
-//   [key: string]: any; // JSON object for each Lottie animation
-// }
+interface Animations {
+  [key: string]: any; // JSON object for each Lottie animation
+}
 
 const WebDesigns: React.FC = () => {
-        // const [animations, setAnimations] = useState<Animations>({});
+        const [animations, setAnimations] = useState<Animations>({});
     
   const [gameSpecs] = useState<GameSpecs[]>([
+        {
+      title: "PetCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2024/03/waGGy.png",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761940921/animals_tnr4xj.json",
+  page_link:"https://themewagon.github.io/waggy/",
+    },
+
+    {
+      title: "PetCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/07/anipat.webp",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761940921/animals_tnr4xj.json",
+  page_link:"https://themewagon.github.io/petsitting/",
+    },
+        {
+      title: "PetCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/11/petsitting-1.webp",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761940921/animals_tnr4xj.json",
+  page_link:"https://themewagon.github.io/anipat/",
+    },
+            {
+      title: "PetCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/06/petcare-1.webp",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761940921/animals_tnr4xj.json",
+  page_link:"https://technext.github.io/petcare/index.html",
+    },
+
+
+            {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2021/12/dentcare-1.png",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/dentcare/",
+    },
+
+                {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2021/08/medic.png",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/medic-care/",
+    },
+                {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2021/06/novena-1.png",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/novena/",
+    },
+                {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/08/Healthcouch.png",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/healthcouch/",
+    },
+                    {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/08/healthcoach.jpg",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/healthcoach/",
+    },
+                    {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/06/acupuncture.jpg",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/acupuncture/",
+    },
+                    {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/01/docmed.webp",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/docmed/",
+    },
+                        {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2018/04/Health-center-ft-Free-HTML5-Bootstrap-Medical-Template-1.jpg",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/health-center/",
+    },
+                        {
+      title: "HealthCare Templates",
+      size: "120 GB",
+      price: "Price",
+      coins: 100,
+      download_link: "",
+      image_link: "https://themewagon.com/wp-content/uploads/2020/06/MedicalCenter-1.jpg",
+      platform: "PC",
+        json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761941257/hospital_mlubpv.json",
+  page_link:"https://themewagon.github.io/medicalcenter/",
+    },
+    
+
+
+
+
+
+
+
+
     {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/judge/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/12/judge.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/judge/",
     },
         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/justlaw/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/09/justlaw.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/justlaw/",
     },
         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://technext.github.io/lawmaker/index.html",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/08/lawmaker-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://technext.github.io/lawmaker/index.html",
     },
         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/lawncare/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/07/lawncare-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/lawncare/",
     },
         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/kanun/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/07/kanun-1.png",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/kanun/",
     },
             {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/notary/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/06/notary.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/notary/",
     },
             {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/law-firm/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/06/law-firm.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/law-firm/",
     },
                 {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/adalot/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/05/adalot-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/adalot/",
     },
                     {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/judicial/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/05/judicial-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/judicial/",
     },
                     {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/thelawyer/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/05/thelawfirm-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/thelawyer/",
     },
                     {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/ariclaw/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/04/ariclaw-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/ariclaw/",
     },
                         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/legalcare/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2020/02/legalcare-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/legalcare/",
     },
                         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/lawyer/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2019/12/lawyer.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/lawyer/",
     },
                         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/jd/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2019/12/JD-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/jd/",
     },
                         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/lawride/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2019/06/lawride-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/lawride/",
     },
                         {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://technext.github.io/lawful/index.html",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2018/12/lawful-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://technext.github.io/lawful/index.html",
     },
                             {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://technext.github.io/lawful/index.html",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2018/12/lawful-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://technext.github.io/lawful/index.html",
     },
                             {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/star-law/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2018/09/StarLaw.webp",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/star-law/",
     },
                             {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/lawfirm/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2018/09/Lawfirm-free-HTML5-lawyer-website-template-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/lawfirm/",
     },
                             {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/law/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2017/11/Law.webp",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/law/",
     },
                                 {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/law/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2017/11/Law.webp",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/law/",
     },
                                     {
       title: "Law Templates",
       size: "120 GB",
       price: "Price",
       coins: 100,
-      download_link: "https://themewagon.github.io/Texas-Lawyer-2/",
+      download_link: "",
       image_link: "https://themewagon.com/wp-content/uploads/2016/05/free-lawyer-attorney-law-firm-website-template1-1.jpg",
       platform: "PC",
+              json_link:"https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json",
+  page_link:"https://themewagon.github.io/Texas-Lawyer-2/",
     },
     
   ]
@@ -263,24 +465,28 @@ const WebDesigns: React.FC = () => {
 //         setGameData(data);
 // };
 
-        //   useEffect(() => {
-        //   // Fetch all JSONs in parallel
-        //   const fetchAnimations = async () => {
-        //     const anims  :any ={};
 
-        //                     try {
-        //           const res = await fetch("https://res.cloudinary.com/dababspdo/raw/upload/v1761926061/law_fffkle.json"); // each course has its JSON URL
-        //           const data = await res.json();
-        //           anims["law"] = data; // store by course id
-        //         } catch (err) {
-        //           console.error("Failed to load animation:", err);
-        //         }
+          useEffect(() => {
+          // Fetch all JSONs in parallel
+          const fetchAnimations = async () => {
+            const anims  :any ={};
+            await Promise.all(
+              gameSpecs.map(async (course) => {
+                try {
+                  const res = await fetch(course.json_link); // each course has its JSON URL
+                  const data = await res.json();
+                  anims[course.title] = data; // store by course id
+                } catch (err) {
+                  console.error("Failed to load animation:", err);
+                }
+              })
+            );
+            setAnimations(anims);
+          };
+      
+          fetchAnimations();
+        }, [gameSpecs]);
 
-
-
-        //     setAnimations(anims);
-        //   };
-        // })
 
 
     return (
@@ -344,7 +550,7 @@ gap:"20px",
       >
       
         {filteredGames.map((data, index) => (
-  <div key={index} className='pc_box'         onClick={() => window.open(data.download_link, "_blank")}>
+  <div key={index} className='pc_box'         onClick={() => window.open(data.page_link, "_blank")}>
     
     <div style={{
         display:"flex",
@@ -381,7 +587,7 @@ gap:"20px",
     width:"30px",
     marginLeft:"10px",
     marginRight: "10px",
-}} animationData= {LawAnime} loop autoplay />
+}} animationData= {animations[data.title]} loop autoplay />
 
     </div>
       {/* <p>size {data.size}</p> */}
